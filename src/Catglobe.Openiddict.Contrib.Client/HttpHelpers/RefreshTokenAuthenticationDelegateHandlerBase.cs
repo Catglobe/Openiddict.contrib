bbf228d99b20ec9c89d7ee00fd7960c9630e1182
Dennis Haney <davh@voxmeter.dk>
2024-03-25 09:22:00 +0700

    BREAKING: add HttpRequestMessage parameter to RefreshTokenAuthenticationDelegateHandlerBase.HandleMissingToken

--- src/Catglobe.Openiddict.Contrib.Client/HttpHelpers/RefreshTokenAuthenticationDelegateHandlerBase.cs
+++ src/Catglobe.Openiddict.Contrib.Client/HttpHelpers/RefreshTokenAuthenticationDelegateHandlerBase.cs
@@ -15,12 +15,17 @@ public abstract class RefreshTokenAuthenticationDelegateHandlerBase : Delegating
       if (!string.IsNullOrEmpty(accessToken))
          request.Headers.Authorization = new("Bearer", accessToken);
       else
-         await HandleMissingToken(cancellationToken);
+         await HandleMissingToken(request, cancellationToken);
       request.Headers.Accept.Add(new("application/json"));
 
       return await base.SendAsync(request, cancellationToken);
    }
-   protected virtual Task HandleMissingToken(CancellationToken cancellationToken) => Task.CompletedTask;
+   /// <summary>
+   /// Default behaviour is to continue with the request, but you can override this to handle the missing token differently
+   /// </summary>
+   /// <param name="request">Request</param>
+   /// <param name="cancellationToken">Token</param>
+   protected virtual Task HandleMissingToken(HttpRequestMessage request, CancellationToken cancellationToken) => Task.CompletedTask;
 
    /// <summary>
    /// Logic to retrieve the current OIDC access token, and refresh if necessary
